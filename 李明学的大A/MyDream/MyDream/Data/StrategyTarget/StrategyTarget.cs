fa51736ba5d934032f8daa4f2764a014b42caa21
mingxuel <mingxuel@noreply.gitcode.com>
2026-01-16 19:47:25 +0800

    Updated.

--- 李明学的大A/MyDream/MyDream/Data/StrategyTarget/StrategyTarget.cs
+++ 李明学的大A/MyDream/MyDream/Data/StrategyTarget/StrategyTarget.cs
@@ -97,8 +97,15 @@ namespace MyDream
                     var record_3 = ZZ5001D.Instance.PreRecord(item.StockCode!, TradingDates.Dates.Last(), 3);
                     var record_4 = ZZ5001D.Instance.PreRecord(item.StockCode!, TradingDates.Dates.Last(), 4);
                     var record_5 = ZZ5001D.Instance.PreRecord(item.StockCode!, TradingDates.Dates.Last(), 5);
-                    //if (record_1!.IsTop || record_2!.IsTop || record_3!.IsTop || record_4!.IsTop || record_5!.IsTop)
-                    if (record_1!.IsTop || record_2!.IsTop || record_3!.IsTop || record_4!.IsTop)
+                    var record_6 = ZZ5001D.Instance.PreRecord(item.StockCode!, TradingDates.Dates.Last(), 5);
+                    var record_7 = ZZ5001D.Instance.PreRecord(item.StockCode!, TradingDates.Dates.Last(), 5);
+                    if (record_7 == null || record_6 == null || record_5 == null || record_4 == null || record_3 == null || record_2 == null || record_1 == null) continue;
+
+                    var ma5 = (record_1.Close + record_2.Close + record_3.Close + record_4.Close + record_5.Close) / 5.0;
+                    var pre_ma5 = (record_2.Close + record_3.Close + record_4.Close + record_5.Close + record_6.Close) / 5.0;
+                    var pre_pre_ma5 = (record_3.Close + record_4.Close + record_5.Close + record_6.Close + record_7.Close) / 5.0;
+                    if ((record_1!.IsTop || record_2!.IsTop || record_3!.IsTop || record_4!.IsTop) &&
+                        record_1.Close > ma5 && record_2.Close > pre_ma5 && record_3.Close > pre_pre_ma5)
                     {
                         StrategyTargetItem target_item = new StrategyTargetItem();
                         target_item.StockName = item.StockName;
@@ -109,7 +116,7 @@ namespace MyDream
             }
 
             DataTwice.Clear();
-            range = 20;
+            range = 10;
             foreach (var stock_code in ZZ500StockCodes.StockCodes)
             {
                 var last_date = TradingDates.Dates.Last();
@@ -117,14 +124,20 @@ namespace MyDream
                 var record_1 = ZZ5001D.Instance[stock_code!]![last_date];
                 var record_2 = ZZ5001D.Instance.PreRecord(stock_code, last_date, 1, true);
                 var record_3 = ZZ5001D.Instance.PreRecord(stock_code, last_date, 2, true);
+                var record_4 = ZZ5001D.Instance.PreRecord(stock_code, last_date, 3, true);
+                var record_5 = ZZ5001D.Instance.PreRecord(stock_code, last_date, 4, true);
+                var record_6 = ZZ5001D.Instance.PreRecord(stock_code, last_date, 5, true);
 
-                if (record_3 == null || record_2 == null || record_1 == null) continue;
+                if (record_6 == null || record_5 == null || record_4 == null || record_3 == null || record_2 == null || record_1 == null) continue;
+
+                var ma5 = (record_1.Close + record_2.Close + record_3.Close + record_4.Close + record_5.Close) / 5.0;
+                var pre_ma5 = (record_2.Close + record_3.Close + record_4.Close + record_5.Close + record_6.Close) / 5.0;
                 if (record_2.Volume < record_1.Volume &&
-                    record_1.IsRed &&
-                    record_2.IsUp && record_1.IsUp &&
+                    record_1.IsRed && record_1.IsUp &&
                     record_2.Low < record_1.Low &&
                     record_2.High < record_1.High &&
-                    !record_2.IsTop && !record_1.IsTop)
+                    !record_2.IsBottom && !record_2.IsTop && !record_1.IsTop && !record_1.IsBottom &&
+                    record_2.Open > pre_ma5 && record_2.Close > pre_ma5 && record_1.Open > ma5 && record_1.Close > ma5)
                 {
                     for (int i = TradingDates.Dates.Count - range; i < TradingDates.Dates.Count - 3; i++)
                     {
