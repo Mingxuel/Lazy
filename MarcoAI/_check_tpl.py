cea6c90a034eaee4b0d9fa23337bcf2359a6271a
mingxuel <mingxuel@noreply.gitcode.com>
2026-08-16 02:56:27 +0800

    Updated

--- MarcoAI/_check_tpl.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import sys
-sys.stdout.reconfigure(encoding="utf-8")
-raw = open(r"e:\Lazy\MarcoAI\AIData\THS\blockstockV3.xml", encoding="utf-8").read()
-print("=== 模板 repr ===")
-print(repr(raw))
-print("\n=== 模板显示 ===")
-print(raw)
